96bac06a995f890e237b49597134ffaa31c496c7
visualstudio <visualstudio@122-PC15>
2019-02-20 10:29:23 -0500

    Commit

--- C0732149-CSD3354-A1/Program.cs
+++ C0732149-CSD3354-A1/Program.cs
@@ -8,8 +8,60 @@ namespace C0732149_CSD3354_A1
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main()
         {
+            UsingRecursion.CalculateRecursively(3, 6);
         }
     }
-}
+   
+class UsingRecursion
+        {
+            // Create an application which 
+            // calculates the sum of all the numbers from n to m recursively:
+           
+          public static int CalculateRecursively(int n, int m)
+            {
+                int sum = n;
+
+                if (n < m)
+                {
+                    n++;
+                    return sum += CalculateRecursively(n, m);
+                }
+                return sum;
+            }
+         
+
+        }
+
+        class CountrySide
+        {
+            // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+            Village Maeland;
+            Village Helmholtz;
+            Village Alst;
+            Village Wessig;
+            Village Badden;
+            Village Uster;
+            Village Schvenig;
+
+
+            class Village
+            {
+                public Village(string _villageName, bool _isAHere)
+                {
+                    isAstrildgeHere = _isAHere;
+                    VillageName = _villageName;
+                }
+
+                public Village west;
+                public Village east;
+                public string VillageName;
+                public int distanceToWestVillage;
+                public int distanceToEastVillage;
+                public bool isAstrildgeHere;
+            }
+        }
+    }
+
+
